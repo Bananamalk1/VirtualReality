b036082d747069fcb3935fa7630ee43d68eda51b
Bananamalk1 <Djin6476@gmail.com>
2025-11-09 18:33:55 +0000

    77

--- VRPractical2_3/house.js
+++ VRPractical2_3/house.js
@@ -1,20 +1,23 @@
-class House{
-    constructor(x,y,z){
+class House {
+    constructor(x, y, z) {
         this.obj = document.createElement("a-entity");
- 
-        let walls = document.createElement("a-box");
-        walls.setAttribute("color","orange");
-        walls.setAttribute("height","2");
-        this.obj.append( walls );
- 
-        let roof = document.createElement("a-cone");
-        roof.setAttribute("position","0 1.5 0");
-        roof.setAttribute("color","brown");
-        roof.setAttribute("radius","0.25");
-        this.obj.append( roof );
- 
-        this.obj.setAttribute("position",{x:x, y:y, z:z});
-        scene.append( this.obj )
 
+        let walls = document.createElement("a-box");
+        walls.setAttribute("color", "lightblue");
+        walls.setAttribute("height", "2");
+        walls.setAttribute("width", "2");
+        walls.setAttribute("depth", "2");
+        walls.setAttribute("position", "0 1 0");
+        this.obj.append(walls);
+
+        let roof = document.createElement("a-cone");
+        roof.setAttribute("position", "0 2.5 0");
+        roof.setAttribute("color", "darkred");
+        roof.setAttribute("radius", "1.2");
+        roof.setAttribute("height", "1");
+        this.obj.append(roof);
+
+        this.obj.setAttribute("position", { x: x, y: y, z: z });
+        scene.append(this.obj);
     }
 }
--- VRPractical2_3/tree.js
+++ VRPractical2_3/tree.js
@@ -1,20 +1,29 @@
-class Tree{
-  constructor(x,y,z){
+class Tree {
+  constructor(x, y, z) {
     this.obj = document.createElement("a-entity");
-  
+
     let pines = document.createElement("a-cone");
-    pines.setAttribute("color","green");
-    pines.setAttribute("position","0 1.5 0");
-    pines.setAttribute("height","2");
-    this.obj.append( pines );
-  
+    pines.setAttribute("color", "darkgreen");
+    pines.setAttribute("position", "0 2.5 0");
+    pines.setAttribute("height", "3");
+    pines.setAttribute("radius-bottom", "1.5");
+    this.obj.append(pines);
+
+    let upperPines = document.createElement("a-cone");
+    upperPines.setAttribute("color", "seagreen");
+    upperPines.setAttribute("position", "0 3.5 0");
+    upperPines.setAttribute("height", "2");
+    upperPines.setAttribute("radius-bottom", "1");
+    this.obj.append(upperPines);
+
     let stump = document.createElement("a-cylinder");
-    stump.setAttribute("position","0 0 0");
-    stump.setAttribute("color","brown");
-    stump.setAttribute("radius","0.25");
-    this.obj.append( stump );
-  
-    this.obj.setAttribute("position",{x:x, y:y, z:z});
-    scene.append( this.obj )
+    stump.setAttribute("position", "0 1 0");
+    stump.setAttribute("color", "saddlebrown");
+    stump.setAttribute("radius", "0.3");
+    stump.setAttribute("height", "2");
+    this.obj.append(stump);
+
+    this.obj.setAttribute("position", { x: x, y: y, z: z });
+    scene.append(this.obj);
   }
 }
