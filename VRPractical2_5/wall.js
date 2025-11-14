eef9a27a8a1134aacf595ad1b12afc38ee0a6abe
Bananamalk1 <Djin6476@gmail.com>
2025-11-14 15:56:43 +0000

    234

--- VRPractical2_5/wall.js
+++ VRPractical2_5/wall.js
@@ -1,7 +1,7 @@
 class Wall {
   constructor(x, y, z) {
     let wall = document.createElement("a-box");
-    wall.setAttribute("color", "blue");  
+    wall.setAttribute("color", "brown");  
     wall.setAttribute("scale", "3 8 3");  
     wall.setAttribute("position", `${x} ${y} ${z}`);
     scene.append(wall);
