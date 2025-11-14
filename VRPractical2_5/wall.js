d2e36325c08fb3cfc51f638c3e2dfb9c13d6a93b
Bananamalk1 <Djin6476@gmail.com>
2025-11-14 15:58:01 +0000

    345

--- VRPractical2_5/wall.js
+++ VRPractical2_5/wall.js
@@ -1,8 +1,8 @@
 class Wall {
   constructor(x, y, z) {
     let wall = document.createElement("a-box");
-    wall.setAttribute("color", "brown");  
-    wall.setAttribute("scale", "3 8 3");  
+    wall.setAttribute("material", "src: #brick-texture; repeat: 2 2"); // use the texture and repeat for tiling
+    wall.setAttribute("scale", "3 8 3");
     wall.setAttribute("position", `${x} ${y} ${z}`);
     scene.append(wall);
   }
