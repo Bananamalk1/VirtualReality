236d7e3fe1541ba4ac33d09cd7a0e120fa826c65
Bananamalk1 <Djin6476@gmail.com>
2025-11-14 15:51:44 +0000

    7

--- VRPractical2_5/wall.js
+++ VRPractical2_5/wall.js
@@ -1,30 +1,9 @@
 class Wall {
   constructor(x, y, z) {
-    this.x = x;
-    this.y = y;
-    this.z = z;
-
-    this.obj = document.createElement("a-entity");
     let wall = document.createElement("a-box");
-    wall.setAttribute("src", "#wall"); 
-    wall.setAttribute("repeat", "0.5 2");
-    wall.setAttribute("width", "4");
-    wall.setAttribute("height", "10");
-    wall.setAttribute("depth", "4");
-    wall.setAttribute("position", { x: 0, y: 0, z: 0 });
-
-    let baseboard = document.createElement("a-box");
-    baseboard.setAttribute("color", "#888");
-    baseboard.setAttribute("width", "4.1");
-    baseboard.setAttribute("height", "0.2");
-    baseboard.setAttribute("depth", "4.1");
-    baseboard.setAttribute("position", { x: 0, y: -5, z: 0 });
-
-    this.obj.append(wall);
-    this.obj.append(baseboard);
-
-    this.obj.setAttribute("position", { x, y, z });
-
-    scene.appendChild(this.obj);
+    wall.setAttribute("color", "blue");  
+    wall.setAttribute("scale", "3 8 3");  
+    wall.setAttribute("position", `${x} ${y} ${z}`);
+    scene.append(wall);
   }
 }
